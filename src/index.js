9a4e574e72eeba628905640731f850ece51d2cf1
@krikun1983 <akhmadeevst@gmail.com>
2021-03-05 21:50:11 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = -n;
+    }
+    let str = n.toString();
+    let str2 = '';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        str2 += str[i];
+    }
+    n = +str2;
+    return n;
 }
